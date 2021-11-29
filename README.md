169491291d36fa293b2a17d405a58a3fdecb77a5
juancinmsb <juanchososa19@gmail.com>
2021-11-28 22:17:00 -0300

    commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Backend-ProyectoSenpai
-Creación del backend de mi proyecto para Senpai Academy...
+# Senpai Academy
+
+## Descripción
+
+Este proyecto nació con la mejor de las ideas y fue creciendo durante cuatro meses.
+Hoy soy un programador feliz :D
+
+## Build project
+
+## Step 1:
+
+```
+npm install
+```
+
+## Step 2:
+
+```
+npm run dev
+```
---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const authRouter = require("./src/routes/auth");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("hola");
+});
+
+app.use("/auth", authRouter);
+
+app.listen(PORT, function () {
+  console.log(`Corriendo en el puerto ${PORT}`);
+});
